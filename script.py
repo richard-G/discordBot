7b2f5c0e95b2f223979f329285d47dc359bbe98c
richard-G <ricardoglass@gmail.com>
2021-01-26 10:48:35 +0000

    code cleanup relating to rl functionality, untested in rl

--- script.py
+++ script.py
@@ -2,7 +2,6 @@
 from discord.ext import commands
 from discord import utils, Intents, FFmpegOpusAudio
 import os
-import time
 import asyncio
 from dotenv import load_dotenv
 
@@ -18,6 +17,26 @@ class CurrentlyPlayingError(Exception):
     pass
 
 
+# TODO
+class track():
+    def __init__(self, name, artist, path, source):
+        self.name = name
+        self.artist = artist
+        self.path = path
+        self.source = source
+
+    def __str__(self):
+        return f'${self.artist} - {self.name}'
+
+    def convert(self):
+        if self.source == 'local':
+            return FFmpegOpusAudio(source=self.path, executable='C:/dev/ffmpeg/bin/ffmpeg.exe')
+
+        # TODO
+        if self.source == 'youtube':
+            pass
+
+
 @bot.event
 async def on_ready():
     print(f'logged in as {bot.user}.')
@@ -25,47 +44,69 @@ async def on_ready():
 
 @bot.event
 async def on_member_update(before, after):
-    if before.name != 'Rich':  # TODO: or rl not in activities
-        print(before.name)
-        print('returning..')
+    if before.name == bot.user:
+        # TODO: test if this condition is ever met
+        print('on_member_update called on bot')
         return
 
-    # TODO: clean up these for loops
-    for activity_0 in before.activities:
-        if activity_0.name == 'Rocket League':
-            break
-    else:
+    # unused
+    # if before.name != 'Rich':  # TODO: or rl not in activities
+    #     print(before.name)
+    #     print('returning..')
+    #     return
+
+    # TODO: test
+    if not is_playing_rl(before, after):
         return
 
-    for activity_1 in after.activities:
-        if activity_1.name == 'Rocket League':
-            break
-    else:
+    # TODO: test
+    if not is_now_losing(before, after):
         return
 
-    print('activity state: ', activity_1.state)
+    try:
+        ctx = await bot.get_context(before)
+        await play_song(ctx)
+    except CurrentlyPlayingError:
+        print('inside CurrentlyPlayingError exception')
+    except Exception as e:
+        print('something went wrong... ', e)
+
+    # # TODO: remove after testing
+    # for activity_0 in before.activities:
+    #     if activity_0.name == 'Rocket League':
+    #         break
+    # else:
+    #     return
+
+    # for activity_1 in after.activities:
+    #     if activity_1.name == 'Rocket League':
+    #         break
+    # else:
+    #     return
+
+    # print('activity state: ', activity_1.state)
 
     # # TODO: test - probably not needed now as we're testing for activity first
     # if not hasattr(activity_0, 'state'):
     #     return
 
-    score_0 = activity_0.state.split()[1]
-    score_1 = activity_1.state.split()[1]
+    # score_0 = activity_0.state.split()[1]
+    # score_1 = activity_1.state.split()[1]
 
-    if is_now_losing(score_0, score_1):
-        try:
-            # TODO: get context here - make sure text channel and voice channel exist on the object else give default val
-            ctx = await bot.get_context(before)
-            await play_song(ctx)
-            # messy, maybe get from user object?  TODO: replace guild and text_channel with ctx
-            guild = bot.guilds[0]
-            text_channel = guild.text_channels[0]
-            await text_channel.send('let\'s gooo!')
-        except CurrentlyPlayingError:
-            print('inside CurrentlyPlayingError block for on_member_update')
-        except Exception as e:
-            print('something went wrong...')
-            print(e)
+    # if is_now_losing(score_0, score_1):
+    #     try:
+    #         # TODO: get context here - make sure text channel and voice channel exist on the object else give default val
+    #         ctx = await bot.get_context(before)
+    #         await play_song(ctx)
+    #         # messy, maybe get from user object?  TODO: replace guild and text_channel with ctx
+    #         guild = bot.guilds[0]
+    #         text_channel = guild.text_channels[0]
+    #         await text_channel.send('let\'s gooo!')
+    #     except CurrentlyPlayingError:
+    #         print('inside CurrentlyPlayingError block for on_member_update')
+    #     except Exception as e:
+    #         print('something went wrong...')
+    #         print(e)
 
 
 @bot.command(name='play')
@@ -93,7 +134,7 @@ async def test(ctx, arg):
     await ctx.send(arg)
 
 
-@bot.command(name='yo', aliases=['summon', 'wag1'])
+@bot.command(name='join', aliases=['yo', 'summon', 'wag1'])
 async def join_call(ctx):
     if not is_connected(ctx.guild):
         vc = ctx.guild.voice_channels[0]        # TODO: generalise this?
@@ -103,7 +144,7 @@ async def join_call(ctx):
         await ctx.send('Already in call...')
 
 
-@bot.command(name='bb', aliases=['fo', 'bye'])
+@bot.command(name='leave', aliases=['bb', 'fo', 'bye'])
 async def leave_call(ctx):
     if is_connected(ctx.guild):
         vc = utils.get(bot.voice_clients, guild=ctx.guild)
@@ -114,23 +155,39 @@ async def leave_call(ctx):
         await ctx.send('Not in call.')
 
 
-def is_now_losing(score0, score1):
+def is_now_losing(before, after):
+    # extract rl activities from member objects
+    rl_0 = next(
+        (activity for activity in before.activities if activity.name == 'Rocket League'), None)
+    rl_1 = next(
+        (activity for activity in after.activities if activity.name == 'Rocket League'), None)
+
+    if not rl_0 and rl_1:
+        print('this condition should never be met (not rl_0 and rl_1)')
+        return False
+
+    # extract scores from rl activities
+    score_0 = rl_0.state.split()[1]
+    score_1 = rl_1.state.split()[1]
+
     # case where score is unchanged
-    if score0 == score1:
+    if score_0 == score_1:
         return False
 
     # case where initial scoreline wasn't even
-    if score0.split(':')[0] != score0.split(':')[1]:
+    if score_0.split(':')[0] != score_0.split(':')[1]:
         return False
 
     # case where we are now winning
-    if int(score1.split(':')[0]) > int(score1.split(':')[1]):
+    if int(score_1.split(':')[0]) > int(score_1.split(':')[1]):
         print('we scored!')
         return False
 
     return True
 
-# TODO: make this a command? or make a command invoke this function
+
+def is_playing_rl(before, after):
+    return any(activity.name == 'Rocket League' for activity in before.activities) and any(activity.name == 'Rocket League' for activity in after.activities)
 
 
 async def play_song(ctx):
